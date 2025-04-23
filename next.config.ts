f2d9667b4b483afbd52717e48bdbf49bafea8c9a
Alberto Dal Dosso <alberto.daldosso@lbaconsulting.eu>
2025-04-23 14:49:13 +0200

    🛠 fix: allow external image domain for Podistica Arona logo in next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["www.podisticaarona.it"],
+  },
 };
 
 export default nextConfig;
