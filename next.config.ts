e5b5ccd3cd3ae209f64c771ff32c70f62749b8f8
Alberto Dal Dosso <alberto.daldosso@lbaconsulting.eu>
2025-04-24 11:22:49 +0200

    🔧 config: allow strava.com image domain for Next.js <Image /> support

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["www.podisticaarona.it"],
+    domains: ["www.podisticaarona.it", "strava.com"],
   },
 };
 
